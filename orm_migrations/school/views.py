28b5b3578e71d76a33b8a83bac0d59688526ac26
DmitriiMurovtsev <mdv12672@gmail.com>
2022-02-07 19:11:04 +0300

    orm task2

--- orm_migrations/school/views.py
+++ orm_migrations/school/views.py
@@ -5,7 +5,7 @@ from .models import Student
 
 
 def students_list(request):
-    object_list = Student.objects.all().order_by('group')
+    object_list = Student.objects.prefetch_related('teacher').order_by('group')
     template = 'school/students_list.html'
     context = {'object_list': object_list}
     return render(request, template, context)
