7fede0bb9f8a098cc9170d205fede772b2d49722
DmitriiMurovtsev <mdv12672@gmail.com>
2022-02-10 13:44:02 +0300

    add validations

--- m2m-relations/articles/admin.py
+++ m2m-relations/articles/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from rest_framework.exceptions import ValidationError
 
 from .models import Article, Scope, ArticleScope
 
 
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        if [form.cleaned_data.get('is_main') for form in self.forms].count(True) == 0:
+            raise ValidationError('Выберите основной раздел')
+        if [form.cleaned_data.get('is_main') for form in self.forms].count(True) > 1:
+            raise ValidationError('Основной раздел должен быть один')
+
+        return super().clean()
+
+
 class ArticleScopeInline(admin.TabularInline):
     model = ArticleScope
+    formset = ArticleScopeInlineFormset
 
 
 @admin.register(Article)
--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -34,4 +34,3 @@ class ArticleScope(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
     tag = models.ForeignKey(Scope, on_delete=models.CASCADE, related_name='tags')
     is_main = models.BooleanField(default=False)
-
